1cf9983fd4e44274e0a8c57c74335cc565fa1a8f
unknown <gaurijkjay@gmail.com>
2026-02-06 13:45:08 +0530

    super

--- src/SuperConstructorDemo.java
+++ src/SuperConstructorDemo.java
@@ -1,16 +1,24 @@
 class Person {
     // TODO: Create a constructor Person()
     // Print "Person class"
+	Person(){
+	     System.out.println("Person class");
+	}
 }
 
 class Student extends Person {
     // TODO: Create a constructor Student()
     // Call super() first (this is implicit, but explicit is good practice)
     // Print "Student class"
+	Student(){
+		super();
+		System.out.println("Student class");
+	}
 }
 
 public class SuperConstructorDemo {
     public static void main(String[] args) {
+	Student s=new Student();
         // TODO: Create an object of Student class
         // Creating the object will automatically invoke constructors
     }
